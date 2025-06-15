262b32bcef6cc5a178c09d242f0a008d36917c68
MostafaSedik020 <mostafa.sedik294@gmail.com>
2025-06-15 04:06:09 +0300

    Add project files.

---
+++ ColMarkSort/Data/Column.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColMarkSort.Data
+{
+    public class Column
+    {
+        public double Width { get; set; }
+        public double Length { get; set; }
+        public string BaseLevel { get; set; }
+        public string TopLevel { get; set; }
+        public double RebarDia { get; set; }
+        public double BarsNumber { get; set; }
+
+
+
+    }
+}
---
+++ ColMarkSort/Entry/ExtCmd.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace ColMarkSort.Entry
+{
+    [Transaction(TransactionMode.Manual)]
+    public class ExtCmd : IExternalCommand
+    {
+        public static UIDocument UIDoc { get; set; }
+        public static Document Doc { get; set; }
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDoc = commandData.Application.ActiveUIDocument;
+            Doc = UIDoc.Document;
+
+            //ExtEventHan = new ExtEventHan();
+            //ExtEvent = ExternalEvent.Create(ExtEventHan);
+
+            //MainWindow mainWindow = new MainWindow(Doc);
+            //mainWindow.ShowDialog();
+            //mainWindow.Show();
+
+            return Result.Succeeded;
+        }
+    }
+}
